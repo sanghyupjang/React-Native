eb27bf95df08f1a4297f3250f4ec86adf9426695
Sanghyup <sanghyeob93@naver.com>
2022-03-05 16:48:41 +0900

    push

--- App.js
+++ App.js
@@ -1,50 +1,67 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Dimensions, StyleSheet, ScrollView } from "react-native";
 
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-    <View style={styles.city}>
-      <Text style={styles.cityName}>Seoul</Text>
-    </View>
-    <View style={styles.weather}>
-      <View style={styles.day}>
-        <Text style={styles.temp}>27</Text>
-        <Text style={styles.description}>Sunny</Text>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
       </View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
-  </View>
-);
+  );
 }
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  backgroundColor: "tomato",
-},
-city: {
-  flex: 1.2,
-  justifyContent: "center",
-  alignItems: "center",
-},
-cityName: {
-  fontSize: 68,
-  fontWeight: "500",
-},
-weather: {
-  flex: 3,
-},
-day: {
-  flex: 1,
-  alignItems: "center",
-},
-temp: {
-  marginTop: 50,
-  fontSize: 178,
-},
-description: {
-  marginTop: -30,
-  fontSize: 60,
-},
+  container: {
+    flex: 1,
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 58,
+    fontWeight: "500",
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontWeight: "600",
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+  },
 });
